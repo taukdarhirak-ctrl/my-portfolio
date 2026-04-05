368f6fe3137fbac0f03e70e5df075074aff82a5e
taukdarhirak-ctrl <taukdarhirak@gmail.com>
2026-04-05 16:55:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+let name = "Hirak";
+let age = 18;
+console.log(`My name is ${name} and I am ${age} years old`);
+console.log(20 + 10); 
+console.log("20" + 10); 
+
+console.log("50" + 10);
+console.log(10 +"50")
+
+
+console.log("5" - 2);
+console.log("5" * 2);
+console.log("6" / 2);
+
+let web = "Hirak's";
+console.log(`Welcome to, ${web} Website`); 
+
+let quality = "bad";
+console.log(`He is a ${quality} boy`);
+
+let price = 200;
+console.log(`I think this clock costs ${price} rupees at most`); 
+
+let thing = "time";
+console.log(`He doesn't know the value of ${thing}`);
+
+let qua = "smart";
+console.log(`Raju is ${qua} enough`);
+console.log(`Raju is ${qua} enough to solve the puzzle`);
+
+let name1 = "Hirak";
+name1 = "Raju";
+console.log(name1);
+
+
+let marks = 50;
+marks = 80;
+console.log(marks); 
+
+let city = "Tezpur";
+city = "Guwahati";
+
+const country = "India";
+
+console.log(city);
+console.log(country); 
+
+
+
+const user = { name: "Raju" };
+user.name = "Ram";
+
+console.log(user.name);
+
+let name5 = "Rahul";
+name5 = "Hirak"; 
+
+console.log(name5);
+
+let myName = "Hirak";
+myName = "Hirak Barman";
+const district = "Baska";
+
+console.log(district);
+console.log(myName);
+
+const user3 = {name: "Raju"};
+user3.name = "Riju";
+
+console.log(user3.name);
+
+
+const car = { brand: "BMW", color: "black" };
+
+car.color = "red";
+car.brand = "TATA";
+
+console.log(car);
+
+let num = 78;
+num = 56;
+
+console.log(num);
+
+const gagust = {name: "Clock" };
+gagust.name = "Watch";
+console.log(gagust.name);
+
+let pen = "Axo";
+pen= "Raitometer";
+
+const company = "Flair";
+
+console.log(pen);
+console.log(company);
+
+let isStudent = true;
+console.log(isStudent);
+
+
+let boat = 3;
+console.log(boat >= 19);
+
+let isLoggedIn = false;
+
+console.log(isLoggedIn); 
+
+let ship = 45;
+
+console.log(ship >= 50);
+
+let studentName = "true";
+console.log(studentName);
+
+let cap = 20;
+console.log(cap >= 15); 
+
+let isPerson = false;
+console.log(isPerson);
+
+let user7 = null;
+console.log(user7); 
+
+
+let sticker = null; 
+console.log(sticker); 
+
+let user9 = null;
+user9 = "Raju";
+
+console.log(user9);
+
+
+let chocolate= null;
+chocolate = 25;
+
+console.log(chocolate);
+
+
+
